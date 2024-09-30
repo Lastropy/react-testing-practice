a61466f50c2166e82988262994e6f2d9f925495b
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-09-30 12:21:11 +0530

    Added Tests for Greet Component

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import {it, expect, describe} from 'vitest';
+import Greet from '../../src/components/Greet';
+import '@testing-library/jest-dom/vitest'
+
+describe('Greet', () => {
+    it('should render name in the heading when name is provided', () => {
+        // Renders our Component in the virtual dom (JSDOM)
+        render(<Greet name="Shivam" />);
+        // See the rendered component on the vitest-ui
+        screen.debug();
+        // Grab a portion of the rendered component
+        const heading = screen.getByRole('heading');
+        // Assertion matchers => jest-dom
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent(/shivam/i);
+    });
+
+    it('should render a login button when name is not provided', () => {
+        render(<Greet name="" />);
+        // Grab a portion of the rendered component
+        const button = screen.getByRole('button');
+        // Assertion matchers => jest-dom
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveTextContent(/login/i);
+    })
+})
+
