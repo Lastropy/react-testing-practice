915d6b1a6677b065d50556dee0f5ac5c9aa1e3d2
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-08 07:12:38 +0530

    Added msw/data and fakerjs to create and manage our mock db on the frontend

---
+++ tests/mocks/db.ts
@@ -0,0 +1,11 @@
+import { factory, primaryKey } from '@mswjs/data'
+import {faker} from '@faker-js/faker'
+
+// Will act as data generator for our mock data
+export const db = factory({
+    product:  {
+        id: primaryKey(() => faker.number.int({min:1, max: 100})),
+        name: faker.commerce.productName,
+        price: () => faker.number.float({min: 1, max: 100})
+    }
+})
