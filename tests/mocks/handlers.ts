e43e54c14ee7f2c016532a79efe83d681e597ea8
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-08 07:33:21 +0530

    Changed Handlers to use mock db handlers

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,20 +1,6 @@
 // This file is used to define our request handlers
-import { http, HttpResponse } from 'msw';
-import { categoriesList, productsList } from './data';
+import { db } from './db';
 
 export const handlers = [
-    http.get('/categories', () => {
-        return HttpResponse.json(categoriesList)
-    }),
-    http.get('/products', () => {
-        return HttpResponse.json(productsList)
-    }),
-    http.get("/products/:id", ({params}) => {
-        const id = parseInt(params.id as string);
-        const product = productsList.find((p) => p.id === id)
-        if(!product) {
-            return new HttpResponse("null", {status: 400});
-        }
-        return HttpResponse.json(product);
-    })
+    ...db.product.toHandlers('rest')
 ]
