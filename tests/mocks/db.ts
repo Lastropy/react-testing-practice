c26a186108454e0bc3d018b2724983744fe6d0a5
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-09 07:56:46 +0530

    Added Category to mock db. Added Handlers for it

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -6,6 +6,11 @@ export const db = factory({
     product:  {
         id: primaryKey(() => faker.number.int({min:1, max: 100})),
         name: faker.commerce.productName,
-        price: () => faker.number.float({min: 1, max: 100})
+        price: () => faker.number.float({min: 1, max: 100}),
+        categoryId: () => faker.number.int({min:1, max: 100})
+    },
+    category: {
+        id: primaryKey(() => faker.number.int({min:1, max: 100})),
+        name: faker.commerce.department
     }
 })
