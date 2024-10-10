cae6abd5ee1bc0e66f6afc347a73e78ae5046536
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2024-10-10 14:01:31 +0530

    Seperated and Refactored CategorySelect and ProductsTable

--- src/pages/BrowseProductsPage.tsx
+++ src/pages/BrowseProductsPage.tsx
@@ -1,114 +1,17 @@
-import { Select, Table } from "@radix-ui/themes";
-import axios from "axios";
 import { useState } from "react";
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import QuantitySelector from "../components/QuantitySelector";
-import { Category, Product } from "../entities";
-import { useQuery } from "react-query";
+import CategorySelect from "../components/CategorySelect";
+import ProductsTable from "../components/ProductTable";
 
 function BrowseProducts() {
-	const categoriesQuery = useQuery<Category[], Error>({
-		queryKey: ["category"],
-		queryFn: () => axios.get<Category[]>("/categories").then((res) => res.data),
-	});
-
-	const productsQuery = useQuery<Product[], Error>({
-		queryKey: ["product"],
-		queryFn: () => axios.get<Product[]>("/products").then((res) => res.data),
-	});
 	const [selectedCategoryId, setSelectedCategoryId] = useState<number | undefined>();
-
-	if (productsQuery.error) return <div>Error: {productsQuery.error.message}</div>;
-
-	const renderCategories = () => {
-		const {
-			isLoading: isCategoriesLoading,
-			error: errorCategories,
-			data: categories,
-		} = categoriesQuery;
-		if (isCategoriesLoading)
-			return (
-				<div role="progressbar" aria-label="loading categories">
-					<Skeleton />
-				</div>
-			);
-		if (errorCategories) return null;
-		return (
-			<Select.Root onValueChange={(categoryId) => setSelectedCategoryId(parseInt(categoryId))}>
-				<Select.Trigger placeholder="Filter by Category" />
-				<Select.Content>
-					<Select.Group>
-						<Select.Label>Category</Select.Label>
-						<Select.Item value="all">All</Select.Item>
-						{categories?.map((category) => (
-							<Select.Item key={category.id} value={category.id.toString()}>
-								{category.name}
-							</Select.Item>
-						))}
-					</Select.Group>
-				</Select.Content>
-			</Select.Root>
-		);
-	};
-
-	const renderProducts = () => {
-		const skeletons = [1, 2, 3, 4, 5];
-		const { data: products, error: errorProducts, isLoading: isProductsLoading } = productsQuery;
-
-		if (errorProducts) return <div>Error: {errorProducts["message"]}</div>;
-
-		const visibleProducts = selectedCategoryId
-			? products!.filter((p) => p.categoryId === selectedCategoryId)
-			: products;
-
-		return (
-			<Table.Root>
-				<Table.Header>
-					<Table.Row>
-						<Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell>Price</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body
-					role={isProductsLoading ? "progressbar" : undefined}
-					aria-label={isProductsLoading ? "loading products" : undefined}
-				>
-					{isProductsLoading &&
-						skeletons.map((skeleton) => (
-							<Table.Row key={skeleton}>
-								<Table.Cell>
-									<Skeleton />
-								</Table.Cell>
-								<Table.Cell>
-									<Skeleton />
-								</Table.Cell>
-								<Table.Cell>
-									<Skeleton />
-								</Table.Cell>
-							</Table.Row>
-						))}
-					{!isProductsLoading &&
-						visibleProducts!.map((product) => (
-							<Table.Row key={product.id}>
-								<Table.Cell>{product.name}</Table.Cell>
-								<Table.Cell>${product.price}</Table.Cell>
-								<Table.Cell>
-									<QuantitySelector product={product} />
-								</Table.Cell>
-							</Table.Row>
-						))}
-				</Table.Body>
-			</Table.Root>
-		);
-	};
-
 	return (
 		<div>
 			<h1>Products</h1>
-			<div className="max-w-xs">{renderCategories()}</div>
-			{renderProducts()}
+			<div className="max-w-xs">
+				<CategorySelect onChange={(cid) => setSelectedCategoryId(cid)} />
+			</div>
+			<ProductsTable selectedCategoryId={selectedCategoryId} />
 		</div>
 	);
 }
